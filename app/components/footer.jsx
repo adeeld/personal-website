fb18b602628076c7afe156f665385dbd52e4d6ef
Adeel Durrani <jeoydurrani2003@gmail.com>
2024-08-21 23:45:35 -0400

    fixed deployment issues and removed blog card

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            Let's connect! <Link target="_blank" href="https://www.linkedin.com/in/adeeld/" className="text-[#16f2b3]">Adeel Durrani</Link>
+            Let&apos;s connect! <Link target="_blank" href="https://www.linkedin.com/in/adeeld/" className="text-[#16f2b3]">Adeel Durrani</Link>
           </p>
           <div className="flex items-center gap-5">
           </div>
